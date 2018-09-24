76123ed11e7c8125b4774044bdf7a94dfc984914
Mark Lewis <mlewis@trinity.edu>
2018-09-24 12:19:09 -0500

    Added Mandelbrot code without parallelism.

--- src/main/scala/basics/JuliaSet.scala
+++ src/main/scala/basics/JuliaSet.scala
@@ -1,23 +1,20 @@
 package basics
 
 import scalafx.Includes._
-import scalafx.application.JFXApp
-import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.Scene
 import scalafx.scene.image.WritableImage
-import scalafx.scene.image.ImageView
+import javafx.scene.image.ImageView
 import scalafx.scene.paint.Color
-import scalafx.scene.input.MouseEvent
 
-object Mandelbrot extends JFXApp {
+class JuliaSet(c: Complex) {
   private var xmin = -1.5
-  private var xmax = 0.5
-  private var ymin = -1.0
-  private var ymax = 1.0
+  private var xmax = 1.5
+  private var ymin = -1.5
+  private var ymax = 1.5
   
   val maxCount = 10000
-  def mandelCount(c: Complex): Int = {
-    var z = new Complex(0, 0)
+  def juliaCount(z0: Complex): Int = {
+    var z = z0
     var cnt = 0
     while (cnt < maxCount && z.mag < 4) {
       z = z * z + c
@@ -34,14 +31,14 @@ object Mandelbrot extends JFXApp {
   def pixelToX(i: Double, img: WritableImage): Double = xmin + i * (xmax - xmin) / img.width()
   def pixelToY(j: Double, img: WritableImage): Double = ymin + j * (ymax - ymin) / img.height()
   
-  def drawMandelbrot(img: WritableImage): Unit = {
+  def drawJulia(img: WritableImage): Unit = {
     val writer = img.pixelWriter
     val start = System.nanoTime()
     for (j <- 0 until img.height().toInt) {
       val y = pixelToY(j, img)
       for (i <- 0 until img.width().toInt) {
         val x = pixelToX(i, img)
-        val cnt = mandelCount(new Complex(x, y))
+        val cnt = juliaCount(new Complex(x, y))
         val color = colorFromCount(cnt)
         writer.setColor(i, j, color)
       }
@@ -49,18 +46,14 @@ object Mandelbrot extends JFXApp {
     println("Took " + ((System.nanoTime() - start) * 1e-9) + " seconds.")
   }
 
-  stage = new PrimaryStage {
-    title = "Mandelbrot"
+  val stage = new scalafx.stage.Stage {
+    title = "Julia for "+c.x+", "+c.y
     scene = new Scene(1000, 1000) {
       val img = new WritableImage(1000, 1000)
       val imgView = new ImageView(img)
       content = imgView
-      drawMandelbrot(img)
-      imgView.onMouseClicked = (e: MouseEvent) => {
-    	  val x = pixelToX(e.x, img)
-        val y = pixelToY(e.y, img)
-        new JuliaSet(new Complex(x, y))
-      }
+      drawJulia(img)
     }
   }
+  stage.show()
 }
