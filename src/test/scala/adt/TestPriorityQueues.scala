b45183e584375c42e22cc1623a7037c03ef7cd42
Mark Lewis <mlewis@trinity.edu>
2018-12-07 15:05:07 -0600

    Binary heap is there.

--- src/test/scala/adt/TestPriorityQueues.scala
+++ src/test/scala/adt/TestPriorityQueues.scala
@@ -5,7 +5,7 @@ import org.junit.Assert._
 
 class TestPriorityQueues {
   @Test def ULLTest: Unit = {
-    val nums = Array.fill(100)(math.random)
+    val nums = Array.fill(10000)(math.random)
     val pq = new UnsortedLLPriorityQueue[Double](_<_)
     for(n <- nums) pq.enqueue(n)
     for(n <- nums.sorted) {
@@ -13,16 +13,24 @@ class TestPriorityQueues {
     }
   }
   @Test def SATest: Unit = {
-    val nums = Array.fill(100)(math.random)
+    val nums = Array.fill(10000)(math.random)
     val pq = new SAPriorityQueue[Double](_<_)
     for(n <- nums) pq.enqueue(n)
     for(n <- nums.sorted) {
       assertEquals(n, pq.dequeue(),0.001)
     }
   }
-  @Test def UATest: Unit = {
-    val nums = Array.fill(100)(math.random)
-    val pq = new UnsortedArrayPQ[Double](_<_)
+//  @Test def UATest: Unit = {
+//    val nums = Array.fill(100)(math.random)
+//    val pq = new UnsortedArrayPQ[Double](_<_)
+//    for(n <- nums) pq.enqueue(n)
+//    for(n <- nums.sorted) {
+//      assertEquals(n, pq.dequeue(),0.001)
+//    }
+//  }
+  @Test def HeapTest: Unit = {
+    val nums = Array.fill(10000000)(math.random)
+    val pq = new BinaryHeap[Double](_<_)
     for(n <- nums) pq.enqueue(n)
     for(n <- nums.sorted) {
       assertEquals(n, pq.dequeue(),0.001)
